ade28d1cd0286e6b7aaac01afad612df23d6e49c
Joe Winter <joe@joeventures.com>
2017-05-14 11:00:00 -0400

    Support for meta

--- phieldbook.php
+++ phieldbook.php
@@ -14,6 +14,7 @@ class PhieldBook {
 	public $api_key;
 	public $api_secret;
 	public $book_id;
+	public $sheet_id;
 	public $table;
 	public $record_id;
 	public $response_info;
@@ -52,6 +53,14 @@ class PhieldBook {
 		curl_setopt($this->ch, CURLOPT_URL, $this->url);
 	}
 
+	private function build_meta_session($meta) {
+		$this->set_url($meta);
+		$this->ch = curl_init();
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->ch, CURLOPT_USERPWD, $this->api_key . ":" . $this->api_secret);
+		curl_setopt($this->ch, CURLOPT_URL, $this->url);
+	}
+
 	private function exec_session() {
 		$this->session = curl_exec($this->ch);
 		$this->response_info = curl_getinfo($this->ch);
@@ -62,27 +71,35 @@ class PhieldBook {
 		return json_decode($this->session, true);
 	}
 
-	private function set_url() {
-		$this->url = implode( '/', array('https://api.fieldbook.com/v1', $this->book_id, $this->table, $this->record_id ));
-		$this->url = rtrim($this->url, '/');
-		if(strlen($this->limit) > 0 || strlen($this->offset) > 0) {
-			if(strpos($this->url, '?') == false) $this->url .= "?";
-			$params = array();
-			if(strlen($this->limit) > 0) $params['limit'] = $this->limit;
-			if(strlen($this->offset) > 0) $params['offset'] = $this->offset;
-			$this->url .= http_build_query($params);
-		}
-		if(count($this->include) > 0 || count($this->exclude) > 0) {
-			if(strpos($this->url, '?')) $this->url .= "&";
-			else $this->url .= "?";
-			if(count($this->include) > 0 && count($this->exclude) == 0) {
-				$fields = implode(",", $this->include);
-				$params = array('include' => $fields);
+	private function set_url($meta = false) {
+		if($meta == 'books') {
+			$this->url = "https://api.fieldbook.com/v1/books/{$this->book_id}";
+		} elseif($meta == 'sheets') {
+			$this->url = "https://api.fieldbook.com/v1/books/{$this->book_id}/sheets";
+		} elseif($meta == 'fields') {
+			$this->url = "https://api.fieldbook.com/v1/sheets/{$this->sheet_id}/fields";
+		} else {
+			$this->url = implode( '/', array('https://api.fieldbook.com/v1', $this->book_id, $this->table, $this->record_id ));
+			$this->url = rtrim($this->url, '/');
+			if(strlen($this->limit) > 0 || strlen($this->offset) > 0) {
+				if(strpos($this->url, '?') == false) $this->url .= "?";
+				$params = array();
+				if(strlen($this->limit) > 0) $params['limit'] = $this->limit;
+				if(strlen($this->offset) > 0) $params['offset'] = $this->offset;
 				$this->url .= http_build_query($params);
-			} elseif(count($this->exclude) > 0 && count($this->include) == 0) {
-				$fields = implode(",", $this->exclude);
-				$params = array('exclude' => $fields);
-				$this->url .= http_build_query($params);
+			}
+			if(count($this->include) > 0 || count($this->exclude) > 0) {
+				if(strpos($this->url, '?')) $this->url .= "&";
+				else $this->url .= "?";
+				if(count($this->include) > 0 && count($this->exclude) == 0) {
+					$fields = implode(",", $this->include);
+					$params = array('include' => $fields);
+					$this->url .= http_build_query($params);
+				} elseif(count($this->exclude) > 0 && count($this->include) == 0) {
+					$fields = implode(",", $this->exclude);
+					$params = array('exclude' => $fields);
+					$this->url .= http_build_query($params);
+				}
 			}
 		}
 	}
@@ -158,6 +175,24 @@ class PhieldBook {
 		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
 		$this->exec_session();
 		return $this->response_info['http_code'];
+	}
+
+	public function book_meta() {
+		$this->build_meta_session('books');
+		$this->exec_session();
+		return $this->session_result();
+	}
+
+	public function sheet_meta() {
+		$this->build_meta_session('sheets');
+		$this->exec_session();
+		return $this->session_result();
+	}
+
+	public function field_meta() {
+		$this->build_meta_session('fields');
+		$this->exec_session();
+		return $this->session_result();
 	}
 
 }
